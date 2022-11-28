af38ddd987f68c04320236f1aa848fc1d666aec7
Michael <zamchalin@mail.ru>
2022-11-28 10:20:04 +0300

    Added task2

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите пожалуйста число: ");
+int n = int.Parse(Console.ReadLine());
+if (n < 100)
+    Console.WriteLine("третьей цифры нет");
+else {
+while ( n >= 1000)
+    n = n / 10;
+    
+}
+Console.WriteLine (n % 10);
