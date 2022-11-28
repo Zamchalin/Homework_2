8be67486784e165e9d411608fbb5dbb35803c17e
Michael <zamchalin@mail.ru>
2022-11-28 10:14:23 +0300

    Added Tak 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.Write("Введите пожалуйста трёхзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int second = a / 10;
+int first = a / 100;
+int result = second - first * 10;
+Console.WriteLine($"{result}");
